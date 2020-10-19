e0b6b2d999c754f15fe49d0c9d9d4a67091732c3
Alex Kuligowski <akuligowski@voceracommunications.com>
2020-10-19 18:51:09 -0400

    WIP of Guessthenumber

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -7,31 +7,25 @@ randomv = 0
 parameters = [1,2,3,4,5,6,7,8,9,10]
 
 #Randomly generating a number into variable with a range from 1-10
-  randomv = random.choice(parameters)
+randomv = random.choice(parameters)
 
 #Get input from user to select an integer from 1-10
 print(Welcome to Guess The Number!)
 print(Please select an integer between 1 and 10:)
-uinput = input()
+uinput = input("Enter value between 1 and 10")
 
-
-
+while uinput != ( 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10):
+	print("Invalid input! Try again with an integer between 1 and 10")
+	uinput = input()
+		
 #Start loop to repeat randomly generated number
 while randomv != uinput:
   
-#Randomly generating a number into variable with a range from 1-10
-  randomv = random.choice(parameters)
-
-#Input of a number from the user in another variable
-  uinput = input("Enter value between 1 and 10")
-  
 #Does random output match random input from user
-
 #If match, show the matching outputs to user
-print(Close but not close enough!)
-print(f'User input: {uinput}')
-print(f'Randomly generated number input: {randomv}')
-print(Try again!)
+	print(Close but not close enough!)
+	print(f'User input: {uinput} did not match random number between 1 and 10')
+	print(Try again!)
 
 #If not match, user can try again to keep match new random number
 print(Good job!)
