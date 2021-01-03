48c1730354786e77337aae8493182b0568fe97a4
Alex Kuligowski <alekuligowski@gmail.com>
2021-01-03 14:18:33 -0500

    LPTHW and Coursera progress

--- JBF/JBF_rd_one.py
+++ JBF/JBF_rd_one.py
@@ -1,50 +1,27 @@
-# RULES FOR IF-STATEMENTS
-# 1. Every if- statement must have an else.
-# 2. If this else should never be run because it doesn’t make sense, then you must use a die
-# function in the else that prints out an error message and dies, just like we did in the last
-# exercise. This will fi nd many errors.
-# 3. Never nest if- statements more than two deep and always try to do them one deep.
-# This means if you put an if in an if, then you should be looking to move that second if
-# into another function.
-# 4. Treat if- statements like paragraphs, where each if, elif, else grouping is like a
-# set of sentences. Put blank lines before and after.
-# 5. Your boolean tests should be simple. If they are complex, move their calculations to variables earlier in your function and use a good name for the variable.
-#
-# RULES FOR LOOPS
-# 1. Use a while- loop only to loop forever, and that means probably never. This only applies
-# to Python; other languages are different.
-# 2. Use a for- loop for all other kinds of looping, especially if there is a fi xed or limited
-# number of things to loop over.
-#
-# TIPS FOR DEBUGGING
-# 1. Do not use a “debugger.” A debugger is like doing a full- body scan on a sick person. You
-# do not get any specifi c useful information, and you fi nd a whole lot of information that
-# doesn’t help and is just confusing.
-# 2. The best way to debug a program is to use print to print out the values of variables at
-# points in the program to see where they go wrong.
-# 3. Make sure parts of your programs work as you work on them. Do not write massive fi les
-# of code before you try to run them. Code a little, run a little, fi x a little.
-
 import random
 import time
 from sys import exit
 
-categories = ['Alternative Comedy', 'Anecdotal Comedy', 'Anti-Humor', 'Dark Comedy', 'Blue Comedy', 'Character Comedy', 'Cringe Comedy',
+categories = ['Alternative Comedy', 'Anecdotal Comedy', 'Anti-Humor', 'Dark Comedy', 'Blue Comedy', 'Character Comedy',
+              'Cringe Comedy',
               'Deadpan Comedy', 'Heritage Comedy', 'Improvisational Comedy', 'Insult Comedy', 'Musical Comedy',
-              'Observational Comedy', 'One-liners', 'Physical Comedy', 'Prop Comedy', 'Shock Humor', 'Sketch Comedy', 'Spoof',
+              'Observational Comedy', 'One-liners', 'Physical Comedy', 'Prop Comedy', 'Shock Humor', 'Sketch Comedy',
+              'Spoof',
               'Surreal Comedy', 'Topical Comedy / Satire', 'Wit / Wordplay']
 scenes = ['Funerals', 'Suicide', 'Sitting at a red light', 'Ghosting', '9/11 - always remember',
           'Your loving partner', 'Drinking', 'Military', 'Fighting', '#metoo movement',
           'Abortion rally', 'Fitness', 'Religion', 'Cancer', 'Fishtanks', 'Candles',
           'Waiting in line at the grocery store', 'Rape', 'Pooping', 'Animals/pets',
-          'Books', 'Armpits','Parents','That one family member...','Siblings','Talking with your crush',
-          'At the dinner table','Cultural norms','Cyclists','Death','Sports','The Homeless',
-          'Poverty','Yoga','Sleeping','Vacation','Technology', 'Government', 'Racism', 'Mental Illness']
+          'Books', 'Armpits', 'Parents', 'That one family member...', 'Siblings', 'Talking with your crush',
+          'At the dinner table', 'Cultural norms', 'Cyclists', 'Death', 'Sports', 'The Homeless',
+          'Poverty', 'Yoga', 'Sleeping', 'Vacation', 'Technology', 'Government', 'Racism', 'Mental Illness']
 t = input("Enter the time in seconds you'd like for this round: ")
+
 
 def retry():
     print("Invalid command, try again!")
     main_menu()
+
 
 def countdown(t):
     while t:
@@ -55,6 +32,7 @@ def countdown(t):
         t -= 1
     print('Nice round!')
 
+
 def round_check():
     next = input("> ")
     if next == "Y" or "Yes" or "yes":
@@ -64,6 +42,7 @@ def round_check():
     else:
         print("Invalid command, try again!")
         round_check()
+
 
 def round():
     round_category = random.choice(categories)
@@ -77,7 +56,8 @@ def round():
     print("For every turn you get one shuffle. Get a new category and round? (Y/N)")
     round_check()
     countdown(int(t))
-    
+
+
 def describe_option(round_category):
     next = input("> ")
     if next == "Y" or "Yes" or "yes":
@@ -88,10 +68,12 @@ def describe_option(round_category):
         print("Invalid command, try again!")
         category_helper()
 
+
 def category_description(round_category):
     next = round_category
     if next == round_category:
         category_description()
+
 
 def example_option(round_category):
     next = input("> ")
@@ -103,23 +85,26 @@ def example_option(round_category):
     else:
         print("Invalid command, try again!")
         category_helper()
-def category_example(round_category):
 
+
+def category_example(round_category):
     next = round_category
     if next == round_category:
         category_description()
+
+
 def display_rules():
     print('''Per turn you get:
     \t* A Time Limit (We recommend two minutes)
     \t* A Category of Comedy
     \t* A Scene or Topic
     ...And just be funny!
-    
+
     Scoring per turn: (4 points possible per round)
     \t* Is it funny (2 points)
     \t* Did you use the category of comedy? (1 point)
     \t* Did you use the scene or topic? (1 point)
-    
+
     In a round, every player gets a turn. First player to 12 points wins. 
     Sudden death if multiple players hit 12 points in a round. 
     For sudden death, a single turn will be taken by every player over 
@@ -127,13 +112,16 @@ def display_rules():
     ''')
     main_menu()
 
+
 def display_scenes():
     for scene in scenes:
         print("\t*%s\n" % scene)
 
+
 def display_categories():
     for category in categories:
         print("\t*%s\n" % category)
+
 
 def start():
     print("Welcome to Just Be Funny!")
@@ -141,9 +129,11 @@ def start():
     print("It looks like you could use a laugh...")
     main_menu()
 
+
 def end_game():
     print("Thanks for playing Just Be Funny!")
     exit()
+
 
 def main_menu():
     print("Select one of the following:")
@@ -169,5 +159,6 @@ def main_menu():
         end_game()
     else:
         retry()
+
 
 start()
---
+++ LPTHW/37 - SymbolReview.py
@@ -0,0 +1,99 @@
+# Keywords
+#  • and - to be used inbetween conditional statements in a loop, if, etc
+#  • del - a way to delete objects, lists, or parts of lists
+#  • from - importing a specific variable, class, or function from a module
+#  • not - implements the negate operator or logical complemenet
+#  • while - execute a set of statements as long as a condition is true
+#  • as - used to create an alias
+#  • elif - else if; an else statement with a conditional operation to be used inside a block
+#  • global - allows a user to modify a variable outside of its local context
+#  • or - a logical operator used to combine conditional statements
+#  • with - used while exception handling to make the code cleaner and much more readable
+#  • assert - used for debugging; lets you test if a condition in your code returns true
+#  • else - the last route of a block
+#  • if - used to create conditional statements and allows you to execute a block of code if a condition is true
+#  • pass - is used as a placeholder for future code; a null statement
+#  • yield - unlike the return statement, is used to turn a function into a generator
+#  • break - terminates the current loop and resumes execution at the next statement
+#  • except - defines a block of code to run if the try block raises an error
+#  • import - finds and loads a package, a sub-package or a module if found in the system
+#  • print - this is a function to output variables, lists, objects, etc
+#  • class - used to create a class, sort of like an object constructor
+#  • exec - executes the dynamically created program, which is either a string or a code object
+#  • in - to check if a value is present in a sequence (list, range, string, etc); also, to iterate through a sequence in a for loop
+#  • raise - used to raise an exception in a block
+#  • continue - used to end the current iteration in a loop and continues to the next iteration
+#  • finally - always executes after leaving the try statement
+#  • is - tests if two variables refer to the same object
+#  • return
+#  • def
+#  • for
+#  • lambda
+#  • try
+# Data Types
+#  • True
+#  • False
+#  • None
+#  • strings
+#  • numbers
+#  • floats
+#  • lists
+# String Esacpe Sequences
+#  • \\
+#  • \'
+#  • \"
+#  • \a
+#  • \b
+#  • \f
+#  • \n
+#  • \r
+#  • \t
+#  • \v
+# String Formats
+#  • %d
+#  • %i
+#  • %o
+#  • %u
+#  • %x
+#  • %X
+#  • %e
+#  • %E
+#  • %f
+#  • %F
+#  • %g
+#  • %G
+# • %c
+#  • %r
+#  • %s
+#  • %%
+# Operators
+#  • +
+#  • -
+#  • *
+#  • **
+#  • /
+#  • //
+#  • %
+#  • <
+#  • >
+#  • <=
+#  • >=
+#  • ==
+#  • !=
+#  • <>
+#  • ( )
+#  •[]
+#  • {}
+#  • @
+#  •,
+#  •:
+#  • .
+#  • =
+#  • ;
+#  • +=
+#  • - =
+#  • *=
+#  • /=
+#  • //=
+#  • %=
+#  • **=
