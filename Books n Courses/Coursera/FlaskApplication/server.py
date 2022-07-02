df35f6842674f45eaf5a2173811447eecaf1fe4f
Alex Kuligowski <alekuligowski@gmail.com>
2022-07-01 22:40:15 -0400

    flask intro

--- Books n Courses/Coursera/FlaskApplication/server.py
+++ Books n Courses/Coursera/FlaskApplication/server.py
@@ -10,3 +10,11 @@ Website: https://imdad.codes
 Instagram: https://www.instagram.com/imdadcodes
 TikTok: https://www.tiktok.com/@imdadahad
 """
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World</p>"
+
