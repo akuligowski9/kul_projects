f843d34dbac79745f4ef992344dde4a7d6d9f4ce
Alex Kuligowski <alekuligowski@gmail.com>
2022-09-15 02:42:42 -0500

    more scenes

--- Harcoppa/JustBeFunny/comedyScenes.py
+++ Harcoppa/JustBeFunny/comedyScenes.py
@@ -1,13 +1,18 @@
-
-scenes = ['Funerals', 'Suicide', 'Sitting at a red light', 'Ghosting', '9/11 - always remember',
-          'Your loving partner', 'Drinking', 'Military', 'Fighting', '#metoo movement',
-          'Abortion rally', 'Fitness', 'Religion', 'Cancer', 'Fishtanks', 'Candles',
-          'Waiting in line at the grocery store', 'Rape', 'Pooping', 'Animals/pets',
-          'Books', 'Armpits', 'Parents', 'That one family member...', 'Siblings', 'Talking with your crush',
-          'At the dinner table', 'Cultural norms', 'Cyclists', 'Death', 'Sports', 'The Homeless',
-          'Poverty', 'Yoga', 'Sleeping', 'Vacation', 'Technology', 'Government', 'Racism', 'Mental Illness']
+scenes = ['Funerals', 'Suicide', 'Sitting at a Red Light', 'Ghosting', '9/11 - Always Remember',
+          'Your Loving Partner', 'Drinking', 'Military', 'Fighting', '#Metoo Movement',
+          'Abortion rally', 'Fitness', 'Religion', 'Cancer', 'Fishtanks',
+          'Waiting in Line at the Grocery Store', 'Rape', 'Pooping', 'Pets',
+          'Books', 'Armpits', 'Parents', 'That One Family Member...', 'Siblings', 'Talking with your Crush',
+          'At the Dinner Table', 'Cultural Norms', 'Cyclists', 'Death', 'Sports', 'The Homeless',
+          'Poverty', 'Yoga', 'Sleeping', 'Vacation', 'Technology', 'Government', 'Racism', 'Mental Illness',
+          'Art Gallery', 'Adoption', 'Antique Store', 'Barbershop', 'Karate Class', 'Yard Sale'
+          'Lost', 'Reincarnation', 'High School', 'College', 'Student Debt', 'Gun Control', 'Birth Control',
+          'Climate Change', 'Kings & Queens', 'Ghosts', 'Aliens', 'Detention', 'Airplanes', 'Shaving',
+          'The Drunk', 'Gay Rights', 'Rest & Relaxation', 'Game Shows', 'Winning the Lottery',
+          'Cafeteria', 'Water']
 
 
 def display_scenes():
     for scene in scenes:
         print("\t*%s\n" % scene)
+
