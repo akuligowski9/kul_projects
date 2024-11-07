53d0d388d760053d3d307290fef55ad768ddce86
Alex Kuligowski <akuligowski@five-startech.com>
2024-11-07 19:48:55 -0300

    remove old JBF repos

--- Harcoppa/JustBeFunny/helperFunctions.py
+++
@@ -1,3 +0,0 @@
-def invalid_command_helper():
-    error = "You've entered an invalid command. Please enter Yes or No. Also Y/N will also work. Try again!"
-    raise AttributeError(error)
--- Harcoppa/JustBeFunny/play_JBF.py
+++
@@ -1,3 +0,0 @@
-from kul_projects.Harcoppa.JustBeFunny.roundNavigation import start_game
-
-start_game()
